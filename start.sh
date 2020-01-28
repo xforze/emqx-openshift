2253924024764a9425667193610c3d930345de37
xforze <ta@ta.vg>
2020-01-28 16:39:55 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 sed "s/emqx@127.0.0.1/emqx@$(hostname -i)/g" -i /etc/emqx/emqx.conf
-sleep 60
+#sleep 60
 /usr/bin/emqx start
 #/usr/bin/emqx_ctl plugins load emqx_backend_redis
 sleep infinity
