e2e82c0186f1956e986cf48d5f855b0354088039
xforze <ta@ta.vg>
2020-02-15 13:01:43 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,6 +2,7 @@
 sed "s/emqx@127.0.0.1/emqx@$(hostname -i)/g" -i /etc/emqx/emqx.conf
 /usr/bin/emqx start
 /pushgateway &
+sleep 10
 /usr/bin/emqx_ctl plugins load emqx_statsd
 /usr/bin/emqx_ctl plugins load emqx_backend_mongo
 /usr/bin/emqx_ctl plugins load emqx_auth_clientid
